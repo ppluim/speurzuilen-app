1f2be43c14bc2f50e9d4d5e0a01952704332cc0e
Pieter Coerman <pcoerman@gmail.com>
2013-12-12 16:45:35 +0100

    Remove image from questions form

--- app/views/partials/questions_form.blade.php
+++ app/views/partials/questions_form.blade.php
@@ -18,24 +18,6 @@
     </div>
 </div>
 
-<div class="control-group">
-    {{ Form::label('image', 'Image') }}
- 
-    <div class="fileupload fileupload-new" data-provides="fileupload">
-        <div class="fileupload-preview thumbnail" style="width: 200px; height: 150px;">
-            @if (Page::find(1)->image)
-                <a href="<?php echo $article->image; ?>"><img src="<?php echo Image::resize($article->image, 200, 150); ?>" alt=""></a>
-            @else
-                <img src="http://www.placehold.it/200x150/EFEFEF/AAAAAA&amp;text=no+image">
-            @endif
-        </div>
-        <div>
-            <span class="btn btn-file"><span class="fileupload-new">Select image</span><span class="fileupload-exists">Change</span>{{ Form::file('image') }}</span>
-            <a href="#" class="btn fileupload-exists" data-dismiss="fileupload">Remove</a>
-        </div>
-    </div>
-</div>
-
 <div class="form-group">
     <div class="col-sm-offset-2 col-sm-6">  
 	  {{ Form::submit('Update', array('class' => 'btn btn-info')) }}
